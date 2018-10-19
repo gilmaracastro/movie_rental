385ac25ef2827b672efeb9c4f87eb3c65b54f7d4
Tássio Caique <tassiok2@gmail.com>
2018-10-19 12:57:13 -0300

    Add - Movie component

---
+++ resources/js/components/Movie.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="movie" @click="handleMovieClick">
+		<img
+			class="cover"
+			:src="cover"/>
+		
+		<div class="title">
+			{{name}}
+		</div>
+		<small class="year">
+			{{year}}
+		</small>
+    </div>
+</template>
+
+<script>
+    export default {
+		props: [
+			'id', 'name', 'year', 'director', 'synopsis', 'cover'
+		],
+        methods: {
+			handleMovieClick() {
+				//Pq não o goTo aqui?
+				this.$emit('click', this.id);
+			}
+		}
+    }
+</script>
+<style scoped>
+	.movie {
+		margin: 5px;
+		padding: 10px;
+		cursor: pointer;
+	}
+	.cover{
+		box-shadow: 1px 1px 10px #000;
+		max-width: 150px;
+	}
+	.title {
+		font-size: 18px;
+		font-weight: bold;
+		color: white;
+	}
+	
+	.year {
+		color:#aaa;
+		display: block;
+	}
+</style>
