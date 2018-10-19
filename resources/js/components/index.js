a4c49c598cd582bd529667e3e1bf0a57da79c92a
Tássio Caique <tassiok2@gmail.com>
2018-10-19 13:00:16 -0300

    Add - Exportação de componentes

---
+++ resources/js/components/index.js
@@ -0,0 +1,13 @@
+import HomePage from './HomePage';
+import DetailsPage from './DetailsPage';
+import EditPage from './EditPage';
+import CreatePage from './CreatePage';
+import NotFound from './NotFound';
+
+export {
+	HomePage,
+	DetailsPage,
+	EditPage,
+	NotFound,
+	CreatePage,
+};
