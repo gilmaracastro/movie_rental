fe425fa18168497843279fab4f32b8ab1b14addd
Tássio Caique <tassiok2@gmail.com>
2018-10-19 12:57:45 -0300

    Add - Componente NavBar

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+	<nav class="navbar navbar-dark bg-dark d-flex flex-row">
+		<input class="search flex-fill" type="search" placeholder="Pesquise pelo nome do filme..." aria-label="Pesquise pelo nome do filme...">
+		<button @click="$emit('click')" class="btn btn-success btn-lg" type="submit">Novo Filme</button>
+	</nav>
+</template>
+<style scoped>
+	.search {
+		border: 0;
+		background-color: #181816;
+		color: white;
+		padding: 10px;
+		font-size: 32px;
+		line-height: 40px;
+		margin-right: 10px;
+	}
+	.navbar-dark {
+		background-color: #181816!important;
+	}
+</style>
