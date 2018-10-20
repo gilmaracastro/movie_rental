e2ad442524983028de4bfc646d78b92eea12ffb7
Tássio Caique <tassiok2@gmail.com>
2018-10-19 23:33:25 -0300

    Adição de eventos

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,9 +1,19 @@
 <template>
 	<nav class="navbar navbar-dark bg-dark d-flex flex-row">
-		<input class="search flex-fill" type="search" placeholder="Pesquise pelo nome do filme..." aria-label="Pesquise pelo nome do filme...">
+		<input @input="$emit('input', filterValue)" v-model="filterValue" class="search flex-fill" type="search" placeholder="Pesquise pelo nome do filme..." aria-label="Pesquise pelo nome do filme...">
 		<button @click="$emit('click')" class="btn btn-success btn-lg" type="submit">Novo Filme</button>
 	</nav>
 </template>
+<script>
+export default {
+	data() {
+		return {
+			filterValue: '',
+		};
+	},
+}
+</script>
+
 <style scoped>
 	.search {
 		border: 0;
@@ -13,6 +23,9 @@
 		font-size: 32px;
 		line-height: 40px;
 		margin-right: 10px;
+		border-color: inherit!important;
+		-webkit-box-shadow: none!important;
+		box-shadow: none!important;
 	}
 	.navbar-dark {
 		background-color: #181816!important;
