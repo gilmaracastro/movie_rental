b5dfab53430575d56292f8e22992b4b0be7ba64e
Tássio Caique <tassiok2@gmail.com>
2018-10-19 23:33:11 -0300

    Add - Roteamento de veirificação de existência do objeto

--- resources/js/components/MovieForm.vue
+++ resources/js/components/MovieForm.vue
@@ -2,6 +2,7 @@
 	<div class="row">
 		<div class="col-4">
 			<image-input-upload
+				:src="form.photo"
 				@base64="handleUpload"
 			/>
 		</div>
@@ -9,12 +10,13 @@
 			<form @submit.prevent="submit">
 				<div class="form-group">
 					<input
-						v-model="form.title"
+						v-model="form.name"
 						type="text"
 						class="form-control"
 						id="movieTitle"
 						aria-describedby="movieHelp"
-						placeholder="Título do filme">
+						placeholder="Título do filme"
+						autofocus>
 				</div>
 				<div class="form-group">
 					<input
@@ -40,10 +42,12 @@
 						class="form-control"
 						id="synopsis"
 						aria-describedby="synopsisHelp"
+						rows="7"
 						placeholder="Sinopse">
 					</textarea>
 				</div>
-				<button type="submit" class="btn btn-success btn-lg">Salvar</button>
+				<button type="submit" class="btn btn-success btn-lg float-right mb-0">Salvar</button>
+				<button @click="handleCancelClick" class="btn btn-outline-light btn-lg float-right mr-2 mb-0">Cancelar</button>
 			</form>	
 		</div>
 	</div>
@@ -56,26 +60,37 @@ export default {
 		ImageInputUpload,
 	},
 
+	props: ['movie'],
+
 	data() {
 		return {
 			form: {
-				title: '',
-				year: '',
-				cover: '',
-				director: '',
-				synopsis: '',
+				name: this.movie ? this.movie.name : null,
+				year: this.movie ? this.movie.year : null,
+				photo: this.movie ? this.movie.photo : null,
+				director: this.movie ? this.movie.director : null,
+				synopsis: this.movie ? this.movie.synopsis : null,
 			},
 		}
 	},
 
+	watch: {
+		movie() {
+			this.form = {...this.movie};
+		}
+	},
+
+
 	methods: {
 		handleUpload(base64) {
-			this.form.cover = base64;
+			this.form.photo = base64;
 		},
 
 		submit() {
-			console.log('submit was called')
 			this.$emit('submit', this.form);
+		},
+		handleCancelClick() {
+			window.location = '/';
 		}
 	}
 }
